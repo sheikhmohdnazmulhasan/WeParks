9fcbc52ca884c27167c621832bf82c9635c7b1cf
mdnazmul42726 <nazmulofficial@outlook.com>
2024-03-03 10:40:37 +0600

    Banner Created

--- app/page.js
+++ app/page.js
@@ -1,8 +1,21 @@
+import bannerImg from '@/public/banner.png';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <main>
-      <p>Hello World</p>
+
+      {/* Banner */}
+      <div className="bg-[url('https://i.ibb.co/XYJy5pR/banner.png')] h-[450px] bg-cover item-left">
+        <div className="bg-black w-full h-full bg-opacity-45 flex items-center p-20">
+          <div className="flex-1"></div>
+          <div className="text-end">
+            <h2 className='text-4xl text-[#29ABE3] font-bold'>Park your car safely <br /> on Airport</h2>
+            <p className='text-white mt-3 font-semibold'>Your gateway to the world</p>
+          </div>
+        </div>
+
+      </div>
     </main>
   )
 }
